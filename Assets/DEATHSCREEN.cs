7f417ed6b09530f0ce6796bb2c00c5b7175d4e62
Svenebanan1 <Casper.aaroe@hotmail.com>
2024-01-16 16:20:08 +0100

    Ljud när man dör

--- Assets/DEATHSCREEN.cs
+++ Assets/DEATHSCREEN.cs
@@ -6,9 +6,11 @@ public class DEATHSCREEN : MonoBehaviour
 {
     [SerializeField] private AudioSource YOUDIED;
     [SerializeField] private AudioSource DED;
+    [SerializeField] private AudioSource BOOM;
     private void audiosource()
     {
         YOUDIED.Play();
         DED.Play();
+        BOOM.Play();
     }
 }
