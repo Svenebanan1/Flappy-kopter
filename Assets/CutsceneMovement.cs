ef9e80d060d6b3bf24e5b4bef583d9d38b7787c5
xXCoochieSlayer69Xx <anton.sellbom@gmail.com>
2024-01-31 13:43:48 +0100

    Ljud för end cutscene

--- Assets/CutsceneMovement.cs
+++ Assets/CutsceneMovement.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class CutsceneMovement : MonoBehaviour
 {
+
+    [SerializeField] public AudioSource kidsCheeringSound;
+
     Rigidbody2D CutScene;
     // Start is called before the first frame update
     void Start()
     {
         CutScene = GetComponent<Rigidbody2D>();
+        kidsCheeringSound.Play();
     }
 
     // Update is called once per frame
